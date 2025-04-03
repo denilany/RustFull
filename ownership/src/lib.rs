b7d2f5ee86bfdeeee2af3cf4ee30a699ebf991da
danyonyi <denilany1@gmail.com>
2025-04-03 15:44:07 +0300

    add ownership package with first_subword function and example usage

---
+++ ownership/src/lib.rs
@@ -0,0 +1,40 @@
+pub fn first_subword(mut s: String) -> String {
+    let mut index = 0;
+    let mut chars = s.chars();
+
+    if let Some(first) = chars.next() {
+        if first.is_ascii_uppercase() {
+            for c in chars {
+                if c.is_ascii_uppercase() {
+                    break;
+                }
+                index += 1;
+            }
+
+            return s[..index+1].to_string();
+        } else if first.is_ascii_lowercase() {
+            for c in chars {
+                if c.is_ascii_uppercase() {
+                    break;
+                } else if c == '_' {
+                    break;
+                }
+                index += 1;
+            }
+
+            return s[..index+1].to_string();
+        }
+    }
+    "".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
