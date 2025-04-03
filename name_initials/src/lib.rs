8083bf9ce3e32a1b9380a48780aa1b54bfc6a0c5
danyonyi <denilany1@gmail.com>
2025-04-03 14:28:49 +0300

    add name_initials package with function to generate initials from names and example usage

---
+++ name_initials/src/lib.rs
@@ -0,0 +1,33 @@
+pub fn initials(names: Vec<&str>) -> Vec<String> {
+    let mut initials_vec: Vec<String> = Vec::new();
+
+    for person in names {
+        let mut initials = String::new();
+
+        for name in person.split_whitespace() {
+            let initial = name.chars().next().unwrap_or_default().to_ascii_uppercase();
+            initials.push(initial);
+            initials.push('.');
+            initials.push(' ');
+        }
+
+        if !initials.is_empty() {
+            initials.pop();
+        }
+
+        initials_vec.push(initials);
+    }
+
+    initials_vec
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
