cd9bf0fb692ab30ef6c0e7b85efc875329a39b29
danyonyi <denilany1@gmail.com>
2025-04-08 15:49:57 +0300

    fix: simplify error handling in open_file function

--- panic/src/lib.rs
+++ panic/src/lib.rs
@@ -1,14 +1,8 @@
 use std::fs::File;
-use std::io::ErrorKind;
 
 pub fn open_file(s: &str) -> File {
     match File::open(s) {
         Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => panic!(),
-            _other_error => {
-                panic!();
-            }
-        },
+        Err(error) => panic!("{}", error)
     }
 }
