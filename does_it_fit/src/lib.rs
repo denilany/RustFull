efa9ce68e3eb2a38480e4e00aa8e8787054f55f2
danyonyi <denilanyonyi1@gmail.com>
2025-04-09 21:59:09 +0300

    refactor: update areas_volumes module usage for improved clarity and consistency

--- does_it_fit/src/lib.rs
+++ does_it_fit/src/lib.rs
@@ -1,12 +1,13 @@
-use areas_volumes::*;
+pub mod areas_volumes;
+pub use areas_volumes::*;
 
 pub fn area_fit(
-	x: usize,
-	y: usize,
-	objects: areas_volumes::GeometricalShapes,
-	times: usize,
-	a: usize,
-	b: usize,
+    x: usize,
+    y: usize,
+    objects: GeometricalShapes,
+    times: usize,
+    a: usize,
+    b: usize,
 ) -> bool {
     let area = x * y;
     let required_area = match objects {
@@ -18,23 +19,25 @@ pub fn area_fit(
 
     (required_area * times as f64) <= area as f64
 }
+
 pub fn volume_fit(
-	x: usize,
-	y: usize,
-	z: usize,
-	objects: areas_volumes::GeometricalVolumes,
-	times: usize,
-	a: usize,
-	b: usize,
-	c: usize,
+    x: usize,
+    y: usize,
+    z: usize,
+    objects: GeometricalVolumes,
+    times: usize,
+    a: usize,
+    b: usize,
+    c: usize,
 ) -> bool {
     let volume = x * y * z;
-    let rectangle_volume = match objects {
+    let required_volume = match objects {
         GeometricalVolumes::Cube => cube_volume(a) as f64,
         GeometricalVolumes::Sphere => sphere_volume(a),
-        GeometricalVolumes::Pyramid => triangular_pyramid_volume(a as f64),
+        GeometricalVolumes::Cone => cone_volume(a, b),
+        GeometricalVolumes::Pyramid => triangular_pyramid_volume(a as f64, b),
         GeometricalVolumes::Parallelepiped => parallelepiped_volume(a, b, c) as f64,
-        };
+    };
 
-        (rectangle_volume * times as f64) <= volume as f64
-    }
+    (required_volume * times as f64) <= volume as f64
+}
