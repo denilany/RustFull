c0cfbd89ccbbcf5b06829ec57539033c450f7fe8
danyonyi <denilany1@gmail.com>
2025-04-09 13:40:57 +0300

    refactor: simplify FormError structure and enhance password validation logic

--- error_types/src/lib.rs
+++ error_types/src/lib.rs
@@ -1,21 +1,19 @@
-use chrono::{DateTime, Utc};
+use chrono::Utc;
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct FormError {
-    pub form_values: (&'static str, String),
+    pub form_values: (String, String),
     pub date: String,
-    pub err: &'static str,
+    pub err: String,
 }
 
 impl FormError {
     pub fn new(field_name: &'static str, field_value: String, err: &'static str) -> Self {
-        let now = Utc::now();
-        let date = now.format("%Y-%m-%d %H:%M:%S").to_string();
-        
-        Self {
-            form_values: (field_name, field_value),
+        let date = Utc::now().format("%Y-%m-%d %H:%M:%S").to_string();
+        FormError {
+            form_values: (field_name.to_string(), field_value),
             date,
-            err,
+            err: err.to_string(),
         }
     }
 }
@@ -27,37 +25,31 @@ pub struct Form {
 }
 
 impl Form {
+    pub fn new(name: String, password: String) -> Self {
+        Form { name, password }
+    }
+
     pub fn validate(&self) -> Result<(), FormError> {
         if self.name.is_empty() {
-            return Err(FormError::new(
-                "name", 
-                self.name.clone(), 
-                "Username is empty"
-            ));
+            return Err(FormError::new("name", self.name.clone(), "Username is empty"));
         }
-        
+
         if self.password.len() < 8 {
-            return Err(FormError::new(
-                "password", 
-                self.password.clone(), 
-                "Password should be at least 8 characters long"
-            ));
+            return Err(FormError::new("password", self.password.clone(), "Password should be at least 8 characters long"));
         }
-        
-        let has_letter = self.password.chars().any(|c| c.is_ascii_alphabetic());
-        let has_number = self.password.chars().any(|c| c.is_ascii_digit());
-        let has_symbol = self.password.chars().any(|c| {
-            c.is_ascii() && !c.is_ascii_alphanumeric()
-        });
-        
-        if !(has_letter && has_number && has_symbol) {
-            return Err(FormError::new(
-                "password", 
-                self.password.clone(), 
-                "Password should be a combination of ASCII numbers, letters and symbols"
-            ));
+
+        if !self.password.chars().any(|c| c.is_ascii_digit()) {
+            return Err(FormError::new("password", self.password.clone(), "Password should be a combination of ASCII numbers, letters and symbols"));
         }
-        
+
+        if !self.password.chars().any(|c| c.is_ascii_alphabetic()) {
+            return Err(FormError::new("password", self.password.clone(), "Password should be a combination of ASCII numbers, letters and symbols"));
+        }
+
+        if !self.password.chars().any(|c| !c.is_ascii_alphanumeric()) {
+            return Err(FormError::new("password", self.password.clone(), "Password should be a combination of ASCII numbers, letters and symbols"));
+        }
+
         Ok(())
     }
 }
