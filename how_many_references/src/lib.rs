74e0e8a536664969006c8a4d1f162733d812ccbb
danyonyi <denilany1@gmail.com>
2025-04-16 16:06:47 +0300

    feat: implement how_many_references module with Node struct and reference counting methods

---
+++ how_many_references/src/lib.rs
@@ -0,0 +1,24 @@
+pub use std::rc::Rc;
+
+pub struct Node {
+    pub ref_list: Vec<Rc<String>>,
+}
+
+impl Node {
+    pub fn new(ref_list: Vec<Rc<String>>) -> Node {
+        Node { ref_list }
+    }
+
+    pub fn add_element(&mut self, element: Rc<String>) {
+        self.ref_list.push(element);
+    }
+
+    pub fn rm_all_ref(&mut self, element: Rc<String>) {
+        // Remove all elements from the vector that point to the same allocation
+        self.ref_list.retain(|x| !Rc::ptr_eq(x, &element));
+    }
+}
+
+pub fn how_many_references(reference: &Rc<String>) -> usize {
+    Rc::strong_count(reference)
+}
