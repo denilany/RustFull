6bc5d6d3b522e262717739c7879406627c819722
danyonyi <denilanyonyi1@gmail.com>
2025-04-19 18:05:21 +0300

    feat: add initial implementation of Vector struct with addition and dot product operations

---
+++ lalgebra_vector/src/main.rs
@@ -0,0 +1,14 @@
+pub struct Vector<T: Scalar>(pub Vec<T>);
+
+use std::ops::Add;
+
+impl Add for Vector<T> {
+}
+
+impl Vector<T> {
+	pub fn new() -> Self {
+	}
+
+	pub fn dot(&self, other: &Self) -> Option<T> {
+    }
+}
