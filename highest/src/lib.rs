8193cfc03bd55edd1aa031a17734c84739f325cc
danyonyi <denilanyonyi1@gmail.com>
2025-04-26 19:54:31 +0300

    feat: add Numbers struct with methods for number manipulation

---
+++ highest/src/lib.rs
@@ -0,0 +1,28 @@
+#[derive(Debug)]
+pub struct Numbers<'a> {
+    numbers: &'a [u32],
+}
+
+impl<'a> Numbers<'a> {
+    pub fn new(numbers: &'a [u32]) -> Self {
+        Numbers { numbers }
+    }
+
+    pub fn list(&self) -> &[u32] {
+        self.numbers
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.numbers.last().copied()
+    }
+
+    pub fn highest(&self) -> Option<u32> {
+        self.numbers.iter().copied().max()
+    }
+
+    pub fn highest_three(&self) -> Vec<u32> {
+        let mut nums = self.numbers.to_vec();
+        nums.sort_unstable_by(|a, b| b.cmp(a));
+        nums.into_iter().take(3).collect()
+    }
+}
